bcdad52c41b96e4458beda09ccd5beb123f53de0
cmdhertel <gianluca.hertel@gmail.com>
2022-01-09 12:16:02 +0100

    add api key to secrets.ini

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 from hue import Hue
+import configparser
 
 def help():
     print("Commands:\n\t1. on & off\n\t2. alarm (repeat, red light\
 on&off)\n\t3. bri (set the brightness 0-254)\n\t4. colors: orange, red, green, blue & white\n") 
 def light(lamp):
-    hue = Hue('AUYosVIdGVuqwefkZ7z8rB4Ni-wh7g84x5KJTFky','192.168.178.25',f'{lamp}')
+    hue = Hue(key ,'192.168.178.25',f'{lamp}')
     help()
     while (command:= input(f'Command(light: {lamp}): ')) != "exit":
         command = command.lower()
@@ -24,13 +25,16 @@ def light(lamp):
             print("Invalid command, try again!")
 
 if __name__ == "__main__":
+    # Load config and secret API-Key
+    config = configparser.ConfigParser()
+    config.read('secrets.ini')
+    global key 
+    key = config['hue']['key']
     while (command:= input("Wich Lamp? ")) != "exit":
+        command = command.lower()
         try:
-            if command == "all":
-                print("es")
-                for n in range(1, 11):
-                    light_[n] = Hue('AUYosVIdGVuqwefkZ7z8rB4Ni-wh7g84x5KJTFky','192.168.178.25',f'{n}')
-                    print(n)
+            if command == "büro":
+                hue = Hue(key ,'192.168.178.25','7')
             else:
                 light(command)
         except:
